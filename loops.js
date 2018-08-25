39b4e6259fd3c2a6b1a404a9805f8d7730367389
Chovin Carlson <chovin@guam.net>
2018-08-25 09:59:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,10 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
-      array.push(`I am ${i} strange loop${i == 0 ? '' : 's'}.`)
+    if (i == 1) {
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
   return array
 }
@@ -13,5 +17,5 @@ function whileLoop(n) {
 }
 
 function doWhileLoop() {
-  
+
 }
